b7c3aca66923ebb068f92b7097e3ea8f5e48e5ea
Jake Magid <jake.magid@gmail.com>
2020-04-22 22:56:56 -0700

    Lecture 111

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button, Dimensions } from 'react-native';
 import MapView from 'react-native-maps';
 
 class MapScreen extends Component {
-    render() {
-        return (
-            <View style={{ flex: 1 }}>
-                <MapView style={{ flex: 1 }} />
-            </View>
-        );
+  state = {
+    region: {
+      longitude: -122,
+      latitude: 37.5,
+      latitudeDelta: 0.4,
+      longitudeDelta: 
+        Dimensions.get("window").width / Dimensions.get("window").height * 0.4
     }
+  }
+
+  onRegionChangeComplete = (region) => {
+    this.setState({ region });
+  }
+
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <MapView 
+          region={this.state.region}
+          style={{ flex: 1 }} 
+          onRegionChangeComplete={this.onRegionChangeComplete}
+        />
+      </View>
+    );
+  }
 }
 
 export default MapScreen;
